2a2c19ef668b78b4e46eb9a008d95e1597879ee9
Nahum Smith <nahumsmith@gmail.com>
2016-06-23 13:07:13 -0400

    create assignment files and start problems

---
+++ Assignment_2.2-Javascript_Basics/js-cheatsheet.js
@@ -0,0 +1,32 @@
+//*****Javascript Cheat Sheet*****
+
+/******Variables*****************/
+
+var greeting = "Hello Everyone!"
+
+
+/******Debugging*****************/
+
+
+
+/******Different Data Types******/
+
+//**Numbers
+
+//**Strings
+
+//**Boolean Values
+
+//**Undefined Values
+
+
+/******Arrays********************/
+
+
+
+/******Testing*******************/
+
+
+
+
+/******Functions*****************/
