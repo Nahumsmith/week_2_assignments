89c7f6352cd251cb6b968a789a149f322a0abb06
Nahum Smith <nahumsmith@gmail.com>
2016-06-24 10:11:07 -0400

    start and finish problem

---
+++ Assignment_2.1-Fizz_Buzz_Simplified/fizzbuzz-problem.js
@@ -0,0 +1,13 @@
+var fizzBuzz = function(num) {
+	
+	if ((num % 3 == 0) && (num % 5 == 0)) {
+		console.log("FizzBuzz")
+	} else if (num % 3 == 0) {
+		console.log("Fizz")
+	} else if (num % 5 == 0) {
+		console.log("Buzz")	
+	} else {
+		console.log("BLAH!")
+	}
+}
+fizzBuzz(32);
